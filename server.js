c794e178230bdb5867670fe118ca70c6ff3d9411
Finbar Maginn <finbar.maginn@dixonscarphone.com>
2017-09-10 23:34:17 +0100

    socket test

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-var express = require('express');
-var app = express();
-var io = require('socket.io')(app);
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.set('port', (process.env.PORT || 5000));
+server.listen(80);
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('index.html');
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
