9e225d7a545a08ce194bb9027ba3585b35d10d7a
Finbar Maginn <finbar.maginn@dixonscarphone.com>
2017-09-10 23:15:22 +0100

    socket test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var io = require("socket.io")(app);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,4 +16,11 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
