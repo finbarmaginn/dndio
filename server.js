204df69a829845a4c3d146bd308f286616b1cc02
Finbar Maginn <finbar.maginn@dixonscarphone.com>
2017-09-10 23:30:08 +0100

    socket test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(app.static(__dirname + '/public'));
 
 // views is directory for all template files
 // app.set('views', __dirname + '/views');
