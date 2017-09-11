1bd6f96c4fcb04af79e13102fa333afb5178c86a
finbarmaginn <finbarmaginn@gmail.com>
2017-09-11 13:00:09 +0100

    refactor naming etc

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = [];
+
+Array.prototype.remove = function() {
+  var what, a = arguments,
+    L = a.length,
+    ax;
+  while (L && this.length) {
+    what = a[--L];
+    while ((ax = this.indexOf(what)) !== -1) {
+      this.splice(ax, 1);
+    }
+  }
+  return this;
+};
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
@@ -8,10 +22,20 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   console.log('a user connected');
+  users.push(socket.id);
+  io.emit('user connected', users)
+
   socket.on('disconnect', function() {
-    console.log('user disconnected');
+    console.log('a user disconnected');
+    users.remove(socket.id)
+    io.emit('user disconnect', users)
   });
+
   socket.on('chat message', function(msg) {
+    // var data = {
+    //   'usr': socket.id,
+    //   'msg': msg
+    // }
     io.emit('chat message', msg)
   })
 });
