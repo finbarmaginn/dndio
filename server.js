60be78dde97738595511ce887280dfd4c1728580
finbarmaginn <finbarmaginn@gmail.com>
2017-09-11 10:59:43 +0100

    log socket

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ var io = require('socket.io')(server);
 
 server.listen((process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
+io.on('connection', function(socket) {
+  console.log(socket);
+  socket.emit('news', {
+    hello: 'world'
+  });
+  socket.on('my other event', function(data) {
     console.log(data);
   });
 });
